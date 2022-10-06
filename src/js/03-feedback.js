cbdd4ad82cb78845e729eabd451de91ca4e8c911
Yuliia Kulish <kulish.yulechka@gmail.com>
2022-10-06 15:53:45 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,33 +3,41 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const inputEmailValue = document.querySelector('input[name=email]');
 const inputTextValue = document.querySelector('textarea[name=message]');
-let formData = {};
 const KEEP_TEXT = 'feedback-form-state';
 
 form.addEventListener('input', throttle((evt => {
   evt.preventDefault();
-  formData = {
+  const objForm = {
     email: inputEmailValue.value,
     message: inputTextValue.value,
   };
-  localStorage.setItem(KEEP_TEXT, JSON.stringify(formData))
+  localStorage.setItem(KEEP_TEXT, JSON.stringify(objForm))
 }), 500));
 
 form.addEventListener('submit', (evt => {
   evt.preventDefault();
   if (inputEmailValue.value !== '' || inputTextValue.value !== '') {
-    console.log(formData);
+    console.log(
+      {
+        email: inputEmailValue.value,
+      message: inputTextValue.value,}
+    );
   }
   evt.currentTarget.reset();
   localStorage.removeItem(KEEP_TEXT);
 }));
 
-savedInputData();
-
-function savedInputData() {
-  const inputData = JSON.parse(localStorage.getItem(KEEP_TEXT));
-  if (inputData) {
-    inputEmailValue.value = inputData.email;
-    inputTextValue.value = inputData.message;
-  };
+const loadValueFormAfterReloadPage = key => {
+  try {
+    const dataFormStorage = localStorage.getItem(key);
+    return dataFormStorage === null ? undefined : JSON.parse(dataFormStorage);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
 };
+
+const checkData = loadValueFormAfterReloadPage(KEEP_TEXT);
+if (checkData) {
+  inputEmailValue.value = checkData.email;
+  inputTextValue.value = checkData.message;
+}
